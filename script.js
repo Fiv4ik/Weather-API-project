80c6e18e902d38a89db8e583d501816d205ee20a
luizagevorkian <66139931+luizagevorkian@users.noreply.github.com>
2026-05-12 08:43:59 +0200

    teachers commit

--- script.js
+++ script.js
@@ -1,1 +1,164 @@
+const API_KEY = 'c38fe24a2f0bdd5584f972b6bd0481e5';
 
+
+
+async function searchWeather() {
+    const city = document.getElementById("cityInput").value.trim();
+
+    if (!city) {
+        alert("Введи місто");
+        return;
+    }
+
+    getWeather(city);
+}
+
+async function getWeather(city) {
+    const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ua`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("Місто не знайдено");
+        }
+
+        const data = await response.json();
+
+        updateUI(data);
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+
+
+function updateUI(data) {
+
+    const city = data.name;
+    const temp = Math.round(data.main.temp);
+    const feels = Math.round(data.main.feels_like);
+    const desc = data.weather[0].description;
+
+    const humidity = data.main.humidity;
+    const wind = Math.round(data.wind.speed * 3.6);
+
+    document.getElementById("city").textContent =
+        `${city}`;
+
+    document.getElementById("temp").textContent =
+        `${temp}°`;
+
+    document.getElementById("feels").textContent =
+        `Відчувається як ${feels}°C`;
+
+    document.getElementById("desc").textContent =
+        desc;
+
+    
+    const stats =
+        document.querySelectorAll(".grid-cols-2 .text-3xl");
+
+    if (stats.length >= 2) {
+        stats[0].textContent = humidity + "%";
+        stats[1].textContent = wind + " км/г";
+    }
+}
+
+function getLocationWeather() {
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        const lat = pos.coords.latitude;
+        const lon = pos.coords.longitude;
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=ua`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        updateUI(data);
+    });
+}
+
+function registerUser() {
+
+    const firstName =
+        document.getElementById("firstName").value.trim();
+
+    const lastName =
+        document.getElementById("lastName").value.trim();
+
+    const email =
+        document.getElementById("email").value.trim();
+
+    const city =
+        document.getElementById("userCity").value.trim();
+
+    const agree =
+        document.getElementById("agree").checked;
+
+    
+    if (!firstName || !lastName || !email || !city) {
+        alert("Заповни всі поля");
+        return;
+    }
+
+    if (!agree) {
+        alert("Погодься з умовами");
+        return;
+    }
+
+    
+    const user = {
+        firstName,
+        lastName,
+        email,
+        city
+    };
+
+    
+    localStorage.setItem(
+        "skyviewUser",
+        JSON.stringify(user)
+    );
+       // Ховаємо реєстрацію
+    document.querySelector(".w-full.lg\\:w-96")
+        .style.display = "none";
+
+    // Показуємо погоду
+    document.getElementById("weatherScreen")
+        .classList.remove("hidden");
+
+     getLocationWeather();
+
+
+    alert(`Вітаємо, ${firstName}! Реєстрація успішна ✅`);
+
+    document.querySelector(".w-full.lg\\:w-96").style.display = "none";
+    console.log(user);
+}
+
+
+
+window.onload = () => {
+
+     // Ховаємо погоду при старті
+    document.getElementById("weatherScreen")
+        .classList.add("hidden");
+
+    // Показуємо форму реєстрації
+    document.querySelector(".w-full.lg\\:w-96")
+        .style.display = "flex";
+          const savedUser =
+        localStorage.getItem("skyviewUser");
+
+    if (savedUser) {
+
+        const user = JSON.parse(savedUser);
+
+        console.log(
+            `Користувач ${user.firstName} вже зареєстрований`
+        );
+    }
+};
